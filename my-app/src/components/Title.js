6d4dab1f2b902d964494ff39c7b0b59f56b73a4d
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-02-19 08:08:54 +0700

    Chỉnnh sửa giao diện

--- my-app/src/components/Title.js
+++ my-app/src/components/Title.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../assets/css/Titlebar.css"
 
 function Title() {
   return (
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,6 +10,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 // Import Font Awesome
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
+import "./assets/css/style.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
